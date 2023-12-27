88c6acda39d55b4c9404b813ec23b82a1101e60d
SohamNandiPDX <snandi@pdx.edu>
2023-12-26 18:49:49 -0800

    last occurance of a character using recursion

---
+++ 06_Recursion/Practice Problems/02_lastOccuranceOfAChar.cpp
@@ -0,0 +1,38 @@
+//Right to Left iteration method
+
+#include <iostream>
+using namespace std;
+
+void lastOccurance(string& str, char key, int i, int& ans){
+    // base case
+    if (i < 0) {
+        return;
+    }
+
+    //1 case solve kar do
+    if(str[i] == key){
+        ans = i;
+        return;
+    }
+
+    //baaki recursion sambhal lega
+    lastOccurance(str,key,i-1,ans);
+}
+
+int main(){
+    cout << "enter your string: ";
+    string str;
+    cin >> str;
+
+    cout << "Enter your key character: ";
+    char key;
+    cin >> key;
+
+    int ans = -1; // variable to store the index of last occurance, initialized with -1.
+    int i = str.size() - 1; // for iteration
+    lastOccurance(str, key, i, ans);
+
+    cout << "The last occurance of " << key << " is in " << ans << "th index";
+
+    return 0;
+}
