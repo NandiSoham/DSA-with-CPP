f33a3d6b56d64e265e726617a01a87daa4c2dc37
SohamNandiPDX <snandi@pdx.edu>
2024-01-13 11:30:04 -0800

    worked on leetcode 152 code

--- 08_OOPs/test.cpp
+++ 08_OOPs/test.cpp
@@ -1,21 +1,28 @@
-#include<bits/stdc++.h>
+
+
+
+#include <bits/stdc++.h>
 using namespace std;
 
-int maxProductSubArray(vector<int>& nums) {
-    int result = nums[0];
-    for(int i=0;i<nums.size()-1;i++) {
-        int p = nums[i];
-        for(int j=i+1;j<nums.size();j++) {
-           result = max(result,p);
-           p *= nums[j];
-        }
-        result = max(result,p);//manages (n-1)th term 
+int maxProductSubArray(vector<int> &arr) {
+    int n = arr.size(); //size of array.
+
+    int pre = 1, suff = 1;
+    int ans = INT_MIN;
+    for (int i = 0; i < n; i++) {
+        if (pre == 0) pre = 1;
+        if (suff == 0) suff = 1;
+        pre *= arr[i];
+        suff *= arr[n - i - 1];
+        ans = max(ans, max(pre, suff));
     }
-    return result;
+    return ans;
 }
 
-int main() {
-    vector<int> nums = {1,2,-3,0,-4,-5};
-    cout<<"The maximum product subarray: "<<maxProductSubArray(nums);
+int main()
+{
+    vector<int> arr = {1, 2, -3, 0, -4, -5};
+    cout << "The maximum product subarray is: "
+         << maxProductSubArray(arr) << "\n";
     return 0;
 }
