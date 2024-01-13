e704c5827ec2f12db5a1cbd5f315efeb4e3417dd
SohamNandiPDX <snandi@pdx.edu>
2024-01-13 11:30:04 -0800

    worked on const with pointer

--- 08_OOPs/05_const.cpp
+++ 08_OOPs/05_const.cpp
@@ -2,9 +2,19 @@
 using namespace std;
 int main()
 {
-    const int x = 5;
+    const int x = 10;
     //initialize can be done but we can,t re-assign a vlue
     // x = 10;
     cout << "value of x is: " << x << endl;
+
+    //const with pointer
+    const int *a = new int(); //const data with NON-CONST pointer
+    cout << "*a initial value: " << *a << endl;
+    // *a = 2;
+    // cout << "*a: " << *a << endl;
+    int b = 5;
+    a = &b;
+    // *a = 20;
+    cout << "Now *a is: " << *a << endl;
     return 0;
 }
