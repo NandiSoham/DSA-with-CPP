920755a52db2400c78ffda5aa1e566ac896c5a2b
SohamNandiPDX <snandi@pdx.edu>
2024-01-13 11:30:04 -0800

    understanding const keyword

---
+++ 08_OOPs/05_const.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    const int x = 5;
+    //initialize can be done but we can,t re-assign a vlue
+    // x = 10;
+    cout << "value of x is: " << x << endl;
+    return 0;
+}
